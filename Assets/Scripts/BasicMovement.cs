a7abc80f2bb12e7fc76ee1fbd25c4b1a19b15766
Guy rahamim <guy.danino5@gmail.com>
2020-12-19 17:36:59 +0200

    Initial Commit

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    float horizontal, vertical, jump;
+    [SerializeField] float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateInputData();
+        Vector3 position = new Vector3(horizontal, jump, vertical);
+        rb.MovePosition(transform.position+ position * speed * Time.deltaTime);
+    }
+
+    void updateInputData()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+    }
+}
---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+    float xScale;
+    [SerializeField] float minScale;
+    [SerializeField] float maxScale;
+    [SerializeField] Vector3 rotation;
+
+    void Start()
+    {
+        xScale = 0;
+    }
+
+    void Update()
+    {
+        scale();
+        rotate();
+        jump();
+    }
+
+    void clamp(ref float val, int min, int max)
+    {
+        if (val > max)
+            val = max;
+        else if (val < min)
+            val = min;
+
+    }
+    void scale()
+    {
+        float step = 0.5f;
+
+        if (transform.localScale.x<minScale)
+            step = 0.5f;
+
+        else if (transform.localScale.x > maxScale)
+            step = -0.5f;
+
+        xScale += step;
+        transform.localScale += new Vector3(xScale * Time.deltaTime, 0, 0);
+    }
+    void rotate()
+    {
+        transform.Rotate(rotation);
+    }
+
+    void jump()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+
+    }
+}
